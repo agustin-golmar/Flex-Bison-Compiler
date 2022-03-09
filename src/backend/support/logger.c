f353ce24ff66706bea560995ce736ca01c27de30
Agustín Golmar <agustin.golmar@gmail.com>
2022-03-09 08:16:14 -0300

    Avoid "va_list" initialization.

--- src/backend/support/logger.c
+++ src/backend/support/logger.c
@@ -13,28 +13,28 @@ void Log(FILE * const stream, const char * prefix, const char * const format, co
 }
 
 void LogDebug(const char * const format, ...) {
-	va_list arguments = 0;
+	va_list arguments;
 	va_start(arguments, format);
 	Log(stdout, "[DEBUG] ", format, "\n", arguments);
 	va_end(arguments);
 }
 
 void LogError(const char * const format, ...) {
-	va_list arguments = 0;
+	va_list arguments;
 	va_start(arguments, format);
 	Log(stderr, "[ERROR] ", format, "\n", arguments);
 	va_end(arguments);
 }
 
 void LogErrorRaw(const char * const format, ...) {
-	va_list arguments = 0;
+	va_list arguments;
 	va_start(arguments, format);
 	Log(stderr, "", format, "", arguments);
 	va_end(arguments);
 }
 
 void LogInfo(const char * const format, ...) {
-	va_list arguments = 0;
+	va_list arguments;
 	va_start(arguments, format);
 	Log(stdout, "[INFO ] ", format, "\n", arguments);
 	va_end(arguments);
