c249442582eb3f9512e9c72be66a541bb53f35f7
Agustín Golmar <agustin.golmar@gmail.com>
2024-03-18 04:27:21 -0300

    Avoid failure the first time.

--- script/linux/build.sh
+++ script/linux/build.sh
@@ -6,9 +6,9 @@ BASE_PATH="$(dirname "$0")/../.."
 cd "$BASE_PATH"
 
 rm --force --recursive "$BASE_PATH/build"
-rm "$BASE_PATH/src/frontend/lexical-analysis/flex-scanner.c"
-rm "$BASE_PATH/src/frontend/syntactic-analysis/bison-parser.c"
-rm "$BASE_PATH/src/frontend/syntactic-analysis/bison-parser.h"
+rm --force "$BASE_PATH/src/frontend/lexical-analysis/flex-scanner.c"
+rm --force "$BASE_PATH/src/frontend/syntactic-analysis/bison-parser.c"
+rm --force "$BASE_PATH/src/frontend/syntactic-analysis/bison-parser.h"
 
 cmake -S . -B build
 cd build
