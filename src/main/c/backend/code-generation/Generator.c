15b16e3aeb87c2c80eac2cf30cd2c098e67d3ca0
Tomás Pietravallo <tpietravallo@itba.edu.ar>
2025-11-23 12:16:58 -0300

    chore(codegen): output over printf

--- src/main/c/backend/code-generation/Generator.c
+++ src/main/c/backend/code-generation/Generator.c
@@ -8,6 +8,8 @@
 #define CDT_NAME_FORMAT "%s_CDT"
 #define CONSTRUCTOR_SUFFIX "_constructor"
 
+const char _indentationCharacter = ' ';
+const char _indentationSize = 4;
 static Logger * _logger = NULL;
 
 void _shutdownGeneratorModule() {
@@ -22,56 +24,76 @@ ModuleDestructor initializeGeneratorModule() {
     return _shutdownGeneratorModule;
 }
 
-static void _emitType(TypeSpecifier * type) {
+static char * _indentation(const unsigned int level) {
+    return indentation(_indentationCharacter, level, _indentationSize);
+}
+
+/**
+ * Outputs a formatted string to standard output. The "fflush" instruction
+ * allows to see the output even close to a failure, because it drops the
+ * buffering.
+ */
+static void _output(const unsigned int indentationLevel, const char * const format, ...) {
+	va_list arguments;
+	va_start(arguments, format);
+	char * indentation = _indentation(indentationLevel);
+	char * effectiveFormat = concatenate(2, indentation, format);
+	vfprintf(stdout, effectiveFormat, arguments);
+	fflush(stdout);
+	free(effectiveFormat);
+	free(indentation);
+	va_end(arguments);
+}
+
+static void _emitType(int indentationLevel, TypeSpecifier * type) {
     if (type == NULL) return;
     switch(type->type) {
-        case INT_TYPE: printf("int"); break;
-        case VOID_TYPE: printf(VOID_KEYWORD); break;
-        case CHAR_TYPE: printf("char"); break;
-        case STRING_TYPE: printf("char *"); break;
-        case IDENTIFIER_TYPE: printf("%s", type->identifier); break;
-        default: printf("unknown_type"); break;
+        case INT_TYPE: _output(indentationLevel, "int"); break;
+        case VOID_TYPE: _output(indentationLevel, VOID_KEYWORD); break;
+        case CHAR_TYPE: _output(indentationLevel, "char"); break;
+        case STRING_TYPE: _output(indentationLevel, "char *"); break;
+        case IDENTIFIER_TYPE: _output(indentationLevel, "%s", type->identifier); break;
+        default: _output(indentationLevel, "unknown_type"); break;
     }
 }
 
-static void _generateTypedefs(Program * program) {
+static void _generateTypedefs(int indentationLevel, Program * program) {
     BlockDeclaration * block = program->blockDeclaration;
     while (block != NULL) {
         if (block->type == CLASS_BLOCK) {
             char * name = block->classDeclaration->identifier;
-            printf("typedef struct " CDT_NAME_FORMAT " *%s;\n", name, name);
+            _output(indentationLevel, "typedef struct " CDT_NAME_FORMAT " *%s;\n", name, name);
         }
         block = block->next;
     }
-    printf("\n");
+    _output(0, "\n");
 }
 
-static void _generateStructs(Program * program) {
+static void _generateStructs(int indentationLevel, Program * program) {
     BlockDeclaration * block = program->blockDeclaration;
     while (block != NULL) {
         if (block->type == CLASS_BLOCK) {
             char * className = block->classDeclaration->identifier;
-            printf("struct " CDT_NAME_FORMAT " {\n", className);
+            _output(indentationLevel, "struct " CDT_NAME_FORMAT " {\n", className);
             
             MemberDeclaration * member = block->classDeclaration->classBody->memberList;
             while (member != NULL) {
                 if (member->type == FIELD_MEMBER) {
                     FieldDeclaration * field = member->fieldDeclaration;
                     if (!field->isStatic) {
-                        printf("    ");
-                        _emitType(field->typeSpecifier);
-                        printf(" %s;\n", field->identifier);
+                        _emitType(indentationLevel + 1, field->typeSpecifier);
+                        _output(0, " %s;\n", field->identifier);
                     }
                 }
                 member = member->next;
             }
-            printf("};\n\n");
+            _output(indentationLevel, "};\n\n");
         }
         block = block->next;
     }
 }
 
-static void _generateStaticGlobals(Program * program) {
+static void _generateStaticGlobals(int indentationLevel, Program * program) {
     BlockDeclaration * block = program->blockDeclaration;
     while (block != NULL) {
         if (block->type == CLASS_BLOCK) {
@@ -81,8 +103,8 @@ static void _generateStaticGlobals(Program * program) {
                 if (member->type == FIELD_MEMBER) {
                     FieldDeclaration * field = member->fieldDeclaration;
                     if (field->isStatic) {
-                        _emitType(field->typeSpecifier);
-                        printf(" %s_%s;\n", className, field->identifier);
+                        _emitType(indentationLevel, field->typeSpecifier);
+                        _output(0, " %s_%s;\n", className, field->identifier);
                     }
                 }
                 member = member->next;
@@ -90,39 +112,39 @@ static void _generateStaticGlobals(Program * program) {
         }
         block = block->next;
     }
-    printf("\n");
+    _output(0, "\n");
 }
 
-static void _emitFunctionSignature(char * className, MethodDeclaration * method, int isStatic, int isConstructor) {
+static void _emitFunctionSignature(int indentationLevel, char * className, MethodDeclaration * method, int isStatic, int isConstructor) {
     if (isConstructor) {
-        if (method->returnType) _emitType(method->returnType);
-        else printf(VOID_KEYWORD);
-        printf(" %s" CONSTRUCTOR_SUFFIX, className);
+        if (method->returnType) _emitType(indentationLevel, method->returnType);
+        else _output(indentationLevel, VOID_KEYWORD);
+        _output(0, " %s" CONSTRUCTOR_SUFFIX, className);
     } else {
-        _emitType(method->returnType);
-        printf(" %s_%s", className, method->identifier);
+        _emitType(indentationLevel, method->returnType);
+        _output(0, " %s_%s", className, method->identifier);
     }
 
-    printf("(");
+    _output(0, "(");
     int first = 1;
     
     if (!isStatic && !isConstructor) {
-        printf("%s this", className);
+        _output(0, "%s this", className);
         first = 0;
     }
 
     Parameter * param = method->parameterList;
     while (param != NULL) {
-        if (!first) printf(", ");
-        _emitType(param->typeSpecifier);
-        printf(" %s", param->identifier);
+        if (!first) _output(0, ", ");
+        _emitType(0, param->typeSpecifier);
+        _output(0, " %s", param->identifier);
         first = 0;
         param = param->next;
     }
-    printf(")");
+    _output(0, ")");
 }
 
-static void _generatePrototypes(Program * program) {
+static void _generatePrototypes(int indentationLevel, Program * program) {
     BlockDeclaration * block = program->blockDeclaration;
     while (block != NULL) {
         if (block->type == CLASS_BLOCK) {
@@ -132,32 +154,32 @@ static void _generatePrototypes(Program * program) {
                 if (member->type == METHOD_MEMBER || member->type == CONSTRUCTOR_MEMBER) {
                     MethodDeclaration * method = member->methodDeclaration;
                     int isConstructor = (member->type == CONSTRUCTOR_MEMBER);
-                    _emitFunctionSignature(className, method, method->isStatic, isConstructor);
-                    printf(";\n");
+                    _emitFunctionSignature(indentationLevel, className, method, method->isStatic, isConstructor);
+                    _output(0, ";\n");
                 }
                 member = member->next;
             }
         } else if (block->type == METHOD_BLOCK) {
              MethodDeclaration * method = block->methodDeclaration;
-             _emitType(method->returnType);
-             printf(" %s(", method->identifier);
+             _emitType(indentationLevel, method->returnType);
+             _output(0, " %s(", method->identifier);
              Parameter * param = method->parameterList;
              int first = 1;
              while (param != NULL) {
-                if (!first) printf(", ");
-                _emitType(param->typeSpecifier);
-                printf(" %s", param->identifier);
+                if (!first) _output(0, ", ");
+                _emitType(0, param->typeSpecifier);
+                _output(0, " %s", param->identifier);
                 first = 0;
                 param = param->next;
              }
-             printf(");\n");
+             _output(0, ");\n");
         }
         block = block->next;
     }
-    printf("\n");
+    _output(0, "\n");
 }
 
-static void _generateBodies(Program * program) {
+static void _generateBodies(int indentationLevel, Program * program) {
     BlockDeclaration * block = program->blockDeclaration;
     while (block != NULL) {
         if (block->type == CLASS_BLOCK) {
@@ -167,25 +189,29 @@ static void _generateBodies(Program * program) {
                 if (member->type == METHOD_MEMBER || member->type == CONSTRUCTOR_MEMBER) {
                     MethodDeclaration * method = member->methodDeclaration;
                     int isConstructor = (member->type == CONSTRUCTOR_MEMBER);
-                    _emitFunctionSignature(className, method, method->isStatic, isConstructor);
-                    printf(" {\n    // TODO: Implementation\n}\n\n");
+                    _emitFunctionSignature(indentationLevel, className, method, method->isStatic, isConstructor);
+                    _output(0, " {\n");
+                    _output(indentationLevel + 1, "// TODO: Implementation\n");
+                    _output(indentationLevel, "}\n\n");
                 }
                 member = member->next;
             }
         } else if (block->type == METHOD_BLOCK) {
              MethodDeclaration * method = block->methodDeclaration;
-             _emitType(method->returnType);
-             printf(" %s(", method->identifier);
+             _emitType(indentationLevel, method->returnType);
+             _output(0, " %s(", method->identifier);
              Parameter * param = method->parameterList;
              int first = 1;
              while (param != NULL) {
-                if (!first) printf(", ");
-                _emitType(param->typeSpecifier);
-                printf(" %s", param->identifier);
+                if (!first) _output(0, ", ");
+                _emitType(0, param->typeSpecifier);
+                _output(0, " %s", param->identifier);
                 first = 0;
                 param = param->next;
              }
-             printf(") {\n    // TODO: Implementation\n}\n\n");
+             _output(0, ") {\n");
+             _output(indentationLevel + 1, "// TODO: Implementation\n");
+             _output(indentationLevel, "}\n\n");
         }
         block = block->next;
     }
@@ -195,11 +221,11 @@ void executeGenerator(CompilerState * compilerState) {
     logDebugging(_logger, "Generating C code...");
     Program * program = compilerState->abstractSyntaxtTree;
     
-    _generateTypedefs(program);
-    _generateStructs(program);
-    _generateStaticGlobals(program);
-    _generatePrototypes(program);
-    _generateBodies(program);
+    _generateTypedefs(0, program);
+    _generateStructs(0, program);
+    _generateStaticGlobals(0, program);
+    _generatePrototypes(0, program);
+    _generateBodies(0, program);
     
     logDebugging(_logger, "Generation done.");
 }
