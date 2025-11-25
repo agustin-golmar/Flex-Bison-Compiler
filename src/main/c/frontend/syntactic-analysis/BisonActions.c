e10fec44063b0768f5203039732a9ff358c370ce
Emilio Mitchell <emitchell@itba.edu.ar>
2025-11-25 15:19:16 -0300

    fix(front): empty arguments on function call now return expression of type empty instead of NULL

--- src/main/c/frontend/syntactic-analysis/BisonActions.c
+++ src/main/c/frontend/syntactic-analysis/BisonActions.c
@@ -825,7 +825,11 @@ Expression * NewExpressionSemanticAction(char * identifier, ArgumentList * argum
 
 ArgumentList * EmptyArgumentListSemanticAction() {
 	_logSyntacticAnalyzerAction(__FUNCTION__);
-	return NULL;
+	ArgumentList* argumentList = calloc(1, sizeof(ArgumentList));
+	argumentList->expression = calloc(1, sizeof(Expression));
+	argumentList->expression->type = EMPTY_EXPRESSION;
+	argumentList->next = NULL;
+	return argumentList;
 }
 
 ArgumentList * ArgumentsSemanticAction(ArgumentList * arguments) {
